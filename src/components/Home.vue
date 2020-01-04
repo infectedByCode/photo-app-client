bb85317f02ad9c8a7e5bec255c58f419cb165635
Matt Daley <mdaleyhk@gmail.com>
2020-01-04 11:38:13 +0000

    Include basic outline of homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,23 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <h3>A photo-sharing site for trips</h3>
-  </div>
+  <main>
+    <section id="jumbotron">
+      <h1>{{ msg }}</h1>
+      <h2>Travel. Enjoy. Share.</h2>
+      <button>Login</button>
+      <a href="#">Not a member? Sign up here.</a>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   props: {
     msg: String
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import "../assets/styles/home.css";
 </style>
