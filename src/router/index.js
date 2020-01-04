44e0a4491925c91c77241b1dec3e3331ae03b18e
Matt Daley <mdaleyhk@gmail.com>
2020-01-04 21:46:52 +0000

    Change router for auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,55 @@
+// import Vue from 'vue';
+// import Router from 'vue-router';
+// import Home from '@/components/Home.vue';
+// import Signup from '@/components/Signup.vue';
+
+// Vue.use(Router);
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     },
+//     {
+//       path: '/signup',
+//       name: 'Signup',
+//       component: Signup
+//     }
+//     // {
+//     // path: '/login',
+//     // name: 'Auth',
+//     // component: Auth
+//     // }
+//   ]
+// });
+
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home.vue';
-import Auth from '@/components/Auth.vue';
+// import firebase from 'firebase';
+
+import Signup from '@/components/Signup';
+import Home from '@/components/Home';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
     {
       path: '/',
       name: 'Home',
-      component: Home
-    },
-    {
-      path: '/signup',
-      name: 'Auth',
-      component: Auth
-    },
-    {
-      path: '/login',
-      name: 'Auth',
-      component: Auth
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
