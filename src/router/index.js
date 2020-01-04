3fae65f52738f60f2028dfce57391a1d0262dc15
Matt Daley <mdaleyhk@gmail.com>
2020-01-04 14:08:28 +0000

    Include auth routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home.vue';
+import Auth from '@/components/Auth.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/signup',
+      name: 'Auth',
+      component: Auth
+    },
+    {
+      path: '/login',
+      name: 'Auth',
+      component: Auth
     }
   ]
 });
