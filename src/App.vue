9f2164ec14689da88e0668b0419a5483aa691dfc
Matt Daley <mdaleyhk@gmail.com>
2020-01-04 17:56:21 +0000

    Remove default styling in #App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
     <router-view :msg="msg"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   components: {},
   data() {
     return {
-      msg: 'trippr'
+      msg: "trippr"
     };
   }
 };
@@ -19,11 +18,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
