4efb4702926435e6569af8c78a2bb97601aa8669
Matt Daley <mdaleyhk@gmail.com>
2020-01-05 15:01:09 +0000

    Remove old code.

--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,3 @@
-// import Vue from 'vue';
-// import Router from 'vue-router';
-// import { currentUser } from '../firebaseConfig';
-
-// import Signup from '@/components/Signup';
-// import Home from '@/components/Home';
-// import Login from '@/components/Login';
-
-// Vue.use(Router);
-
-// const router = new Router({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'Home',
-//       component: Home,
-//       meta: {
-//         requiresAuth: true
-//       }
-//     },
-//     {
-//       path: '/login',
-//       name: 'Login',
-//       component: Login,
-//       meta: {
-//         requiresGuest: true
-//       }
-//     },
-//     {
-//       path: '/signup',
-//       name: 'Signup',
-//       component: Signup,
-//       meta: {
-//         requiresGuest: true
-//       }
-//     }
-//   ]
-// });
-
-// router.beforeEach((to, from, next) => {
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//     if (!currentUser) {
-//       next({
-//         path: '/login',
-//         query: {
-//           redirect: to.fullPath
-//         }
-//       });
-//     } else {
-//       next();
-//     }
-//   } else if (to.matched.some(record => record.meta.requiresGuest)) {
-//     if (currentUser) {
-//       next({
-//         path: '/',
-//         query: {
-//           redirect: to.fullPath
-//         }
-//       });
-//     } else {
-//       next();
-//     }
-//   } else {
-//     next();
-//   }
-// });
-
-// export default router;
-
 import Vue from 'vue';
 import Router from 'vue-router';
 import firebase from 'firebase';
@@ -109,7 +38,7 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   const currentUser = firebase.auth().currentUser;
-
+  alert(currentUser);
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!currentUser) {
       next({
