90b8b788555f8e5e45a2bdac4be11d93bbb4a796
Matt Daley <mdaleyhk@gmail.com>
2020-01-05 12:21:00 +0000

    Add error handling when user login error

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,11 @@
 <template>
   <main id="auth">
-    <form
-      @submit="
+    <form @submit="
         e => {
           loginUser(e, email, password);
         }
-      "
-    >
-      <h1 class="logo">
-        Share your trips
-      </h1>
+      ">
+      <h1 class="logo">Share your trips</h1>
 
       <label>
         Email:
@@ -31,26 +27,26 @@
       </label>
 
       <button>Login</button>
-      <p class="error">
-        {{ errEmail }}
-      </p>
+      <p class="error">{{ errEmail || errLogin }}</p>
+      <router-link v-if="errLogin.length" to="/signup">Not already registered? Click here</router-link>
     </form>
   </main>
 </template>
 
 <script>
-import { checkEmail } from '../utils/utils';
-import { loginUser } from '../auth';
+import { checkEmail } from "../utils/utils";
+import { loginUser } from "../auth";
 
 export default {
-  name: 'Auth',
+  name: "Auth",
   data() {
     return {
       // Errors
-      errEmail: '',
+      errEmail: "",
+      errLogin: "",
       // Inputs
-      email: '',
-      password: ''
+      email: "",
+      password: ""
     };
   },
   methods: {
@@ -61,5 +57,5 @@ export default {
 </script>
 
 <style scoped>
-@import '../assets/styles/auth.css';
+@import "../assets/styles/auth.css";
 </style>
