e30ea7467236e1dcce81e37a61d61ba88447bdf2
Matt Daley <mdaleyhk@gmail.com>
2020-01-05 14:36:39 +0000

    Include NavBar component for each page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <router-link class="logo" to="/">trippr</router-link>
+    <button @click="url = !url" v-if="!url">
+      <router-link to="/login" key="login">Login</router-link>
+    </button>
+    <button @click="url = !url" v-if="url">
+      <router-link to="/signup" key="signup">Register</router-link>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      url: window.location.pathname === "/login"
+    };
+  },
+  methods: {
+    // handleLink:funct
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/styles/main.css";
+</style>
