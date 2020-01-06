eaeed51aabb857ce7b80c02a48e6380c77520c04
Matt Daley <mdaleyhk@gmail.com>
2020-01-06 13:54:45 +0000

    Create outline of location single page view

---
+++ src/components/Location.vue
@@ -0,0 +1,29 @@
+<template>
+  <main id="home">
+    <section id="jumbotron">
+      <h1>trippr</h1>
+      <h2>Travel. Enjoy. Yay.</h2>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Location",
+
+  mounted() {
+    setTimeout(() => {
+      this.$store.state.isLoading = false;
+    }, 100);
+  },
+  methods: {
+    setLoading: function() {
+      this.$store.state.isLoading = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "../assets/styles/home.css";
+</style>
