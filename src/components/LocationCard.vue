5dd82a7e82e2b477de9e88f8406861629168f4b7
Matt Daley <mdaleyhk@gmail.com>
2020-01-06 13:55:26 +0000

    Add LocationCard component for displaying all locations on Locations component.

--- src/components/LocationCard.vue
+++ src/components/LocationCard.vue
@@ -1,19 +1,24 @@
 <template>
   <ul id="display">
-    <li v-for="location in locations" v-bind:key="location.location_id">
+    <router-link
+      v-for="location in locations"
+      v-bind:key="location.location_id"
+      :to="{ name: 'Location', params: { location_id: location.location_id }}"
+    >
       <h1>{{location.city}}</h1>
       <img :src="location.image_url" alt />
       <h2>{{location.country}}</h2>
       <h3>{{ location.continent}}</h3>
-    </li>
+    </router-link>
   </ul>
 </template>
 
 <script>
 export default {
+  name: "LocationCard",
   props: ["locations"]
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
