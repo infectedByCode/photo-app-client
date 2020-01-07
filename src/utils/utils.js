16af2f8598597199fe0e37d5e6b4ccc59b9a9905
Matt Daley <mdaleyhk@gmail.com>
2020-01-07 17:40:41 +0000

    Update regex for country special characters

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -58,7 +58,7 @@ function checkInputs(e, input) {
   else this.errInput = "Please only use alphanumeric characters and special characters . , & ' and space.";
 }
 function checkLocationName(e, input) {
-  const validCharacters = /([^A-Z\d' ])/gi;
+  const validCharacters = /([^A-Z\d()Å-ôé' ])/gi;
 
   if (!input.length || !validCharacters.test(input)) this.errLocation = '';
   else this.errLocation = "Please only use alphanumeric characters and special characters ' and space.";
