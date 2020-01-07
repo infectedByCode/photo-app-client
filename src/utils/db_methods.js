e861522ef014952ad2d1f9ea8d722758808757e9
Matt Daley <mdaleyhk@gmail.com>
2020-01-07 15:48:21 +0000

    Extract db function calls to improve readability of ReviewForm

---
+++ src/utils/db_methods.js
@@ -0,0 +1,63 @@
+import * as api from '../api';
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  if (this.review) {
+    const current = this.review;
+
+    const review_title = !this.reviewTitle.length ? current.review_title : this.reviewTitle;
+    const review_body = !this.reviewBody.length ? current.review_body : this.reviewBody;
+
+    api
+      .updateReviewByID(current.review_id, review_title, review_body)
+      .then(review => {
+        if (review) this.$router.replace('../../locations');
+      })
+      .catch(err => {
+        if (err) this.errDB = 'Something went wrong, please try again.';
+      });
+  } else {
+    const locationRequest = {
+      city: this.reviewCity,
+      country: this.reviewCountry,
+      continent: this.reviewContinent,
+      image_url: this.imageURL
+    };
+
+    api
+      .postLocation(locationRequest)
+      .then(location_id => {
+        const postRequest = {
+          review_title: this.reviewTitle,
+          review_body: this.reviewBody,
+          image_url: this.imageURL,
+          author: this.author,
+          location_id: location_id
+        };
+
+        api.postReview(postRequest).then(review => {
+          if (review) alert('posted');
+        });
+      })
+      .catch(err => {
+        if (err) this.errDB = err;
+      });
+  }
+}
+
+function fetchReviewByID(review_id) {
+  api
+    .getReviewByID(review_id)
+    .then(review => {
+      this.review = review;
+      this.reviewTitle = review.review_title;
+      this.reviewBody = review.review_body;
+      this.formStyle = 'review-form-page';
+    })
+    .catch(err => {
+      if (err) this.errDB = 'Something went wrong.';
+    });
+}
+
+export { handleFormSubmit, fetchReviewByID };
